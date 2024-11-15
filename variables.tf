901a0f09f7ca051780c45b6f8ab25291cec1d46f
Alex Ballesta <aballesta@bodas.net>
2024-11-15 13:39:53 +0100

    abc for variables

--- variables.tf
+++ variables.tf
@@ -303,19 +303,19 @@ variable "listener_rules" {
   description = "List of listener rules"
   type = map(object({
     action            = optional(string)
-    priority          = optional(number)
-    target_group_arn  = optional(string)
-    host_header_values = optional(list(string))
-    path_pattern_values = optional(list(string))
-    http_header_values = optional(list(object({
-      name   = string
-      values = list(string)
-    })))
     fixed_response    = optional(object({
       content_type = string
       message_body = string
       status_code  = string
     }))
+    host_header_values = optional(list(string))
+    http_header_values = optional(list(object({
+      name   = string
+      values = list(string)
+    })))
+    path_pattern_values = optional(list(string))
+    priority          = optional(number)
+    target_group_arn  = optional(string)
   }))
 }
 
